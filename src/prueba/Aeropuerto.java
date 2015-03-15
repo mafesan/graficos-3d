b19ab92d058cd53e24d5956340c91d21920b71fc
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-03-15 19:59:31 +0100

    Añadido array de dibujables. Añadidos mas aviones

--- src/prueba/Aeropuerto.java
+++ src/prueba/Aeropuerto.java
@@ -14,7 +14,6 @@ package prueba;
 public class Aeropuerto {
 
     /**
-     * @param args the command line arguments
      */
 
     /* Proximos pasos para la práctica:
@@ -24,20 +23,37 @@ public class Aeropuerto {
      * a una en concreto aterricen, y la pista va diciendo si 
      * hay algun avion en ella.
      */
-    public static void main(float pos_y) {
+    
+    public static float pos_y = -2.5f;
+    
+    public static void main() {
+       
+       Dibujable dibujables[] = new Dibujable[10];
+       pos_y += 0.01;
+       
        Fondo miPaisaje = new Fondo();
+       dibujables[0] = miPaisaje;
        Pista pistaMain = new Pista();
+       dibujables[1] = pistaMain;
        Pista pista2 = new Pista();
        Torre torreControl = new Torre();
-       Avion miBoeing = new Avion(0f, pos_y-1.5f, 0f);
+       dibujables[2] = torreControl;
+       Avion miBoeing1 = new Avion(0f, pos_y-1.5f, 0f);
+       Avion miBoeing2 = new Avion(0.4f, pos_y-1.8f, 0f);
+       Avion miBoeing3 = new Avion(-0.4f, pos_y-1.8f, 0f);
+       Avion miBoeing4 = new Avion(0.8f, pos_y-2f, 0f);
+       Avion miBoeing5 = new Avion(-0.8f, pos_y-2f, 0f);
        
+       dibujables[3] = miBoeing1;
+       dibujables[4] = miBoeing2;
+       dibujables[5] = miBoeing3;
+       dibujables[6] = miBoeing4;
+       dibujables[7] = miBoeing5;
        
        //Dibujamos los elementos
-       miPaisaje.Draw();
-       torreControl.Draw();
-       pistaMain.Draw();
-       
-       miBoeing.Draw();
+       for(int dex = 0; dex <= 7; dex++){
+           dibujables[dex].Draw();
+       }
        
        //Interactuamos con ellos
        miPaisaje.AddElements();
@@ -53,11 +69,11 @@ public class Aeropuerto {
        /*
        pista2.Draw();
        pista2.EsPrincipal("Otra");
-       */
+       
        miBoeing.TakeOff();
        miBoeing.PlaneSpeed();
        miBoeing.LandPlane();
-
+        */
     }
     
 }
